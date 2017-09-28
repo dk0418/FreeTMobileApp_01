a3deb54f11439a4f7f895998dd27ce9e2d95ec8a
moonggi <heartboy08@nate.com>
2017-09-28 18:17:08 +0900

    문기2(이미지) (퇴근전 수정)

--- app/src/main/java/com/example/pc/tabmenutest2/HomeFragment.java
+++ app/src/main/java/com/example/pc/tabmenutest2/HomeFragment.java
@@ -7,12 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 
 public class HomeFragment extends Fragment implements View.OnClickListener
 {
     String url ="http://m.freet.co.kr/mevent/event_201709.jsp";
+    String url2 ="http://m.freet.co.kr/mevent/event_201708.jsp";
+    Intent it;
+    Button leftbutton , rightbutton;
     public HomeFragment()
     {
         // Required empty public constructor
@@ -24,7 +28,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_home, container, false);
         ImageView imageevent = (ImageView)view.findViewById(R.id.imageevent);
+        ImageView imageevent2 = (ImageView)view.findViewById(R.id.imageevent2);
+        leftbutton = (Button)view.findViewById(R.id.imageleft_button);
+        rightbutton = (Button)view.findViewById(R.id.imageright_button);
         imageevent.setOnClickListener(this);
+        imageevent2.setOnClickListener(this);
+        leftbutton.setOnClickListener(this);
+        rightbutton.setOnClickListener(this);
         return view;
     }
 
@@ -35,9 +45,14 @@ public class HomeFragment extends Fragment implements View.OnClickListener
         switch (view.getId())
         {
             case R.id.imageevent:
-                Intent it = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
                 startActivity(it);
                 break;
+            case R.id.imageevent2:
+                it = new Intent(Intent.ACTION_VIEW, Uri.parse(url2));
+                startActivity(it);
+                break;
+
         }
 
     }
